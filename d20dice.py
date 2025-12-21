8dcffe5f4550580370acb76d5e5a3009973b076a
ImBad <70207769+neofilisoft@users.noreply.github.com>
2025-12-21 23:23:36 +0700

    Update d20dice.py

--- d20dice.py
+++ d20dice.py
@@ -3,6 +3,10 @@ import random
 def roll_d20():
     result = random.randint(1, 20)
     print(f"d{result}")
+    if result == 20:
+        print("ritical Success!")
+    elif result == 1:
+        print("Critical Fail!")
     return result
 
 if __name__ == "__main__":
